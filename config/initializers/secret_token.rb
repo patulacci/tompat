8e2bee58db4a27d7b3e980435e8dd9c154e9d9a0
PATOUILLAUD <thomas.patouillaud@gmail.com>
2011-09-04 01:10:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tompat::Application.config.secret_token = '07491df05e46de06b009a5ca618b4a4d27a993a8986c3e7f123afebea007f5b922136d76826d49023748fb5f692bae4fdc5a57a3506a30943246f2bf5d437e95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tompat::Application.config.session_store :cookie_store, :key => '_tompat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tompat::Application.config.session_store :active_record_store
