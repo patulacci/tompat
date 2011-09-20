35b07dd6a99ce6957b9aa193e3798ed7abcc91df
PATOUILLAUD <thomas.patouillaud@gmail.com>
2011-09-20 20:18:58 +0200

    tab design

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,37 +4,48 @@ $(document).ready(
 
 	$('li#li_education').click(function(event){
 		event.preventDefault();
-		$('div#home').fadeOut(1000);
-		$('div#videos').fadeOut(1000);
-		$('div#photos').fadeOut(1000);
-		$('div#education').fadeIn(1000);
-		$('div#title h1#big_title').text('Education');
+		$('section#home').fadeOut(1000);
+		$('section#videos').fadeOut(1000);
+		$('section#photos').fadeOut(1000);
+		$('section#education').fadeIn(1000);
+		$(' h1#big_title').text('Education');
 		}),
 		
 		$('li#li_home').click(function(event){
 			event.preventDefault();
-			$('div#education').fadeOut(1000);
-			$('div#photos').fadeOut(1000);
-			$('div#videos').fadeOut(1000);
-			$('div#home').fadeIn(1000);
-			$('div#title h1#big_title').text('Home');
+			$('section#education').fadeOut(1000);
+			$('section#photos').fadeOut(1000);
+			$('section#videos').fadeOut(1000);
+			$('section#home').fadeIn(1000);
+			$(' h1#big_title').text('Home');
 		}),
 		
 		$('li#li_videos').click(function(event){
 			event.preventDefault();
-			$('div#home').fadeOut(1000);
-			$('div#education').fadeOut(1000);
-			$('div#photos').fadeOut(1000);
-			$('div#videos').fadeIn(1000);
-			$('div#title h1#big_title').text('Videos');
+			$('section#home').fadeOut(1000);
+			$('section#education').fadeOut(1000);
+			$('section#photos').fadeOut(1000);
+			$('section#videos').fadeIn(1000);
+			$(' h1#big_title').text('Videos');
 		}),
 		
 		$('li#li_photos').click(function(event){
 			event.preventDefault();
-			$('div#home').fadeOut(1000);
-			$('div#education').fadeOut(1000);
-			$('div#videos').fadeOut(1000);
-			$('div#photos').fadeIn(1000);
-			$('div#title h1#big_title').text('Photos');
+			$('section#home').fadeOut(1000);
+			$('section#education').fadeOut(1000);
+			$('section#videos').fadeOut(1000);
+			$('section#photos').fadeIn(1000);
+			$(' h1#big_title').text('Photos');
 		});
 });
+
+
+$(function() {
+$( "#selectable" ).selectable();
+});
+
+
+
+
+
+
